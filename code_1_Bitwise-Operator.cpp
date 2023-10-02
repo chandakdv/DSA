ea35d0df6adfb8e26a2859af4276c91f7d0f7d9d
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2023-10-02 11:22:49 +0530

    Add files via upload

---
+++ code_1_Bitwise-Operator.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=4;
+    int b=6;
+    
+    cout<<"a&b"<<(a&b)<< endl;
+    cout<<"a | b"<<(a |b )<< endl;
+    cout<<"~a"<<(~a)<< endl;
+    cout<<"a^b"<<(a^b)<< endl;
+
+cout<< (17>>1)<<endl;
+cout << (17>>2)<<endl;
+cout << (19<<1) << endl;
+cout << (19<<2)<<endl;
+
+int i =7;
+cout << (++i)<< endl;
+cout << (i++)<<endl;
+cout << (i--) << endl;
+cout << (--i)<< endl;
+
+
+    return 0;
+}
