10c11fbc514b65076c271e0e0644efb11ef61c2f
Divya  Chandak <108147421+chandakdv@users.noreply.github.com>
2023-10-02 11:23:53 +0530

    Add files via upload

---
+++ code_5_binary_to_Decimal.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin>>n;
+   int i=0,ans=0;
+   while(n!=0){
+       int digit = n%10;
+       if(digit ==1){
+           ans = ans +pow(2,i);
+       }
+       n = n/10;
+       i++;
+   
+       cout<<ans<<endl;
+   }
+}
